7c71891e131f959279d9240dabdbb1a866a1bdd6
C-Fruit-Stu <124798605+C-Fruit-Stu@users.noreply.github.com>
2024-02-04 20:46:58 +0200

    Update Notes.tsx

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -3,7 +3,7 @@ export default function Notes({ notes }: NotesList) {
     const sortedListNotes = notes?.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
     return (
         <>
-        
+          <h2>Notes:</h2>
             <ul>
                 {
                     sortedListNotes?.length>0?
